edf8c50773e2bbafb10dabc7b55cffde52bd7f11
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-04-14 21:33:41 +0300

    Create part-y-0073-array6.rs

---
+++ part-y-0073-array6.rs
@@ -0,0 +1,34 @@
+fn main() {
+
+    let some_nums = [-12i8, 0, 33, 24, 47, -5, 100];
+
+    let (e, o) = return_vecs(&some_nums);
+
+    println!("Even numbers: {:?}", &e);
+
+    println!("Odd numbers: {:?}", &o);
+}
+
+fn return_vecs(numbers: &[i8]) -> (Vec<i8>, Vec<i8>) {
+
+    let mut evens: Vec<i8> = Vec::new();
+
+    let mut odds: Vec<i8> = Vec::new();
+
+    for num in numbers {
+
+        if num % 2 == 0 {
+
+            evens.push(*num);
+
+        } else {
+            
+            odds.push(*num);
+        }
+    }
+
+    (evens, odds)
+}
+
+// Even numbers: [-12, 0, 24, 100]
+// Odd numbers: [33, 47, -5]
