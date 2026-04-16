0b547d4785da7b130093ea3bc20cbe108d414d07
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-04-16 20:46:05 +0300

    Create part-y-0090-for-loop-enumerate1.rs

---
+++ part-y-0090-for-loop-enumerate1.rs
@@ -0,0 +1,13 @@
+fn main() {
+
+    for (i, num) in (10u8..=14).enumerate() {
+
+        println!("Number {}: {}", i + 1, num);       
+    }
+}
+
+// Number 1: 10
+// Number 2: 11
+// Number 3: 12
+// Number 4: 13
+// Number 5: 14
